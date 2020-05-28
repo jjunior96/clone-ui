7ace7884aa0d5a8eedaea8b13823c8174b7fdff3
Junior <jjalves96@gmail.com>
2020-05-28 18:29:05 -0300

    feat: created component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.HeaderWrapper>
+      <h1>Header</h1>
+    </S.HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.section`
+  height: 80px;
+  padding: 0 30px;
+  background: #7159c1;
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+`;
